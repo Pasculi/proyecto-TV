c810181f84c494da3533e37136c6dc0ca6836402
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-12-18 14:57:43 -0300

    feat:Ajuste responsivo de DetailsCard

--- backendtv/app.js
+++ backendtv/app.js
@@ -13,14 +13,37 @@ const TipoMulticast = require("./routes/tipoMulticast.route");
 const Contact = require("./routes/contact.route");
 const { mongoose } = require("./config/config.mongoose");
 const morgan = require("morgan");
-const cors = require("cors");
+
 /* const corsOptions = {
   origin: 'https://example.com',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type',
 }; */
+const cors = require("cors");
+const allowedCors = [
+  "http://localhost:8000",
+  "http://localhost:8001",
+  "http://localhost:8000",
+  "http://localhost:8000/manager",
+  "http://localhost:8000/admin",
+];
 
-app.use(cors());
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Obtener el origen de la solicitud
+  const { method } = req; // Obtener el método HTTP de la solicitud
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    const requestHeaders = req.headers["access-control-request-headers"];
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    return res.end();
+  }
+  next();
+});
+
 app.use(helmet());
 app.use(morgan("dev"));
 

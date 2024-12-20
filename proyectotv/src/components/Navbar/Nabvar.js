5c00498b49be534163415ebf416eea312490d8ff
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-12-20 15:48:01 -0300

    feat:Subida de enlaces

--- proyectotv/src/components/Navbar/Nabvar.js
+++ proyectotv/src/components/Navbar/Nabvar.js
@@ -13,7 +13,7 @@ const Nabvar = ({ nombre }) => {
               Inicio
             </Link>
           </li>
-          {nombre.nombre !== "Jorge" ? (
+          {/* {nombre.nombre !== "Jorge" ? (
             <li>
               <Link className="navbar-links-item" to="/admin">
                 Admin
@@ -37,7 +37,7 @@ const Nabvar = ({ nombre }) => {
             </>
           ) : (
             " "
-          )}
+          )} */}
         </ul>
       </div>
     </>
